02cdb3c7b12f03d6bc0e2f2df5ea09d310559e8f
Jéssica Pamplona De Almeida <romulopamplona958@gmail.com>
2023-07-04 17:41:55 -0300

    Adicionado o css ao projeto, e realizado alguns ajustes no js

--- Script.js
+++ Script.js
@@ -1,68 +1,57 @@
 function result() {
   const currencyOne = document.getElementById("currency-one").value;
   const currencyTwo = document.getElementById("currency-two").value;
-  const valorMultiplicado = parseFloat(document.getElementById("value").value);
+  const conversionValue = parseFloat(document.getElementById("value").value);
   const spanElement = document.getElementById("conversion-result");
 
   if ((currencyOne === "BRL" || currencyOne === "USD" || currencyOne === "EUR" || currencyOne === "GBP")
   &&(currencyTwo === "BRL" || currencyTwo === "USD" || currencyTwo === "EUR" || currencyTwo === "GBP")
-  &&(valorMultiplicado > 0)) {
-    fetch(`https://economia.awesomeapi.com.br/json/last/BRL-USD,USD-BRL,BRL-EUR,EUR-BRL,BRL-GBP,GBP-BRL`).then(response => {
+  &&(conversionValue > 0)) {
+    fetch(`https://economia.awesomeapi.com.br/json/last/BRL-USD,USD-BRL,BRL-EUR,EUR-BRL,BRL-GBP,GBP-BRL,USD-EUR,EUR-USD,USD-GBP,GBP-USD,EUR-GBP,GBP-EUR`).then(response => {
       return response.json()
-    }).then(corpo => {
+    }).then(data => {
       if ((currencyOne === "BRL")&&(currencyTwo === "USD")) { 
-        const resultado = corpo.BRLUSD.bid * valorMultiplicado;
-        spanElement.textContent = resultado;
+        const result = (data.BRLUSD.bid * conversionValue).toFixed(2);
+        spanElement.textContent = result;
       } else if ((currencyOne === "USD")&&(currencyTwo === "BRL")) {
-        const resultado = corpo.USDBRL.bid * valorMultiplicado;
-        spanElement.textContent = resultado;
+        const result = (data.USDBRL.bid * conversionValue).toFixed(2);
+        spanElement.textContent = result;
       } else if ((currencyOne === "BRL")&&(currencyTwo === "EUR")) {
-        const resultado = corpo.BRLEUR.bid * valorMultiplicado;
-        spanElement.textContent = resultado;
+        const result = (data.BRLEUR.bid * conversionValue).toFixed(2);
+        spanElement.textContent = result;
       } else if ((currencyOne === "EUR")&&(currencyTwo === "BRL")) {
-        const resultado = corpo.EURBRL.bid * valorMultiplicado;
-        spanElement.textContent = resultado;
+        const result = (data.EURBRL.bid * conversionValue).toFixed(2);
+        spanElement.textContent = result;
       } else if ((currencyOne === "BRL")&&(currencyTwo === "GBP")) {
-        const resultado = corpo.BRLGBP.bid * valorMultiplicado;
-        spanElement.textContent = resultado;
+        const result = (data.BRLGBP.bid * conversionValue).toFixed(2);
+        spanElement.textContent = result;
       } else if ((currencyOne === "GBP")&&(currencyTwo === "BRL")) {
-        const resultado = corpo.GBPBRL.bid * valorMultiplicado;
-        spanElement.textContent = resultado;
+        const result = (data.GBPBRL.bid * conversionValue).toFixed(2);
+        spanElement.textContent = result;
+      } else if ((currencyOne === "USD")&&(currencyTwo === "EUR")) {
+        const result = (data.USDEUR.bid * conversionValue).toFixed(2);
+        spanElement.textContent = result;
+      } else if ((currencyOne === "EUR")&&(currencyTwo === "USD")) {
+        const result = (data.EURUSD.bid * conversionValue).toFixed(2);
+        spanElement.textContent = result;
+      } else if ((currencyOne === "USD")&&(currencyTwo === "GBP")) {
+        const result = (data.USDGBP.bid * conversionValue).toFixed(2);
+        spanElement.textContent = result;
+      } else if ((currencyOne === "GBP")&&(currencyTwo === "USD")) {
+        const result = (data.GBPUSD.bid * conversionValue).toFixed(2);
+        spanElement.textContent = result;
+      } else if ((currencyOne === "EUR")&&(currencyTwo === "GBP")) {
+        const result = (data.EURGBP.bid * conversionValue).toFixed(2);
+        spanElement.textContent = result;
+      } else if ((currencyOne === "GBP")&&(currencyTwo === "EUR")) {
+        const result = (data.GBPEUR.bid * conversionValue).toFixed(2);
+        spanElement.textContent = result;
       } else if ((currencyOne === "BRL" && currencyTwo === "BRL")||(currencyOne === "USD" && currencyTwo === "USD")
       ||(currencyOne === "EUR" && currencyTwo === "EUR")||(currencyOne === "GBP" && currencyTwo === "GBP")) {
-        spanElement.textContent = valorMultiplicado;
+        spanElement.textContent = conversionValue;
       }
     })
   } else {
     spanElement.textContent = "Por favor, selecione as moedas e preencha o valor.";
   }
 }
-  
-
-
-
-/*function result() {
-  const currencyOne = document.getElementById("currency-one").value;
-  const currencyTwo = document.getElementById("currency-two").value;
-  const valorMultiplicado = parseFloat(document.getElementById("value").value);
-  const spanElement = document.getElementById("conversion-result");
-
-  if ((currencyOne === "BRL" || currencyOne === "USD")&&(currencyTwo === "BRL" || currencyTwo === "USD")&&(valorMultiplicado > 0)) {
-    if ((currencyOne === "BRL")&&(currencyTwo === "USD")){
-      fetch(`https://economia.awesomeapi.com.br/json/last/USD-BRL,BRL-USD`).then(response => {
-        return response.json()
-      }).then(corpo => {
-        const resultado = corpo.USDBRL.bid * valorMultiplicado;
-        spanElement.textContent = resultado;
-      })
-    } else if ((currencyOne === "USD")&&(currencyTwo === "BRL")) {
-      fetch(`https://economia.awesomeapi.com.br/json/last/BRL-USD`).then(response => {
-        return response.json()
-      }).then(corpo => {
-        const resultado = corpo.BRLUSD.bid * valorMultiplicado;
-        spanElement.textContent = resultado;
-      })}
-  } else {
-    spanElement.textContent = "Por favor, selecione uma moeda e preencha o valor.";
-  }
-}*/
